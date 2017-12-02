7419c12b6deb0bb973e5918e7ca943c27355bb3f
daniruiz <daniruizdealegria@gmail.com>
2017-12-02 08:54:57 +0100

    dynamic content loads by php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-$uri = $_SERVER['REQUEST_URI'];
 
-include_once("index.html");
+function get_content_dir(){
+    $uri = $_SERVER['REQUEST_URI'];
+    $dir = "";
+    $config = file_get_contents("config");
+    $config = str_replace("\\","\\\\",$config);
+    $config_json = json_decode($config, true);
+    foreach ($config_json as $pattern => $value){   
+        if(preg_match($pattern, $uri)) {
+            $dir = $value;
+            break;
+        }
+    }
+    return "content/".$dir;
+}
+
+function create_html($content_dir){
+    $index_html = file_get_contents("index.html");
+    $content_html = file_get_contents($content_dir);
+    echo str_replace("<{ content }>", $content_html, $index_html);
+}
+
+$content_dir = get_content_dir();
+create_html($content_dir);
 ?>
