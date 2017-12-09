ad433069351b3101509a68c58cceb58af9fc4b39
daniruiz <daniruizdealegria@gmail.com>
2017-12-09 13:02:13 +0100

    FIRST RELEASE v1.0; Add: onpopstate page change

--- js/index.js
+++ js/index.js
@@ -1,32 +1,42 @@
 ( function( config ) {
 	'use strict';
 	
-	var linkElements = document.getElementsByClassName( '__eth-link' );
-	loadLinks( linkElements );
+	(function(){
+		var linkElements = document.getElementsByClassName( '__eth-link' );
+		loadLinks( linkElements );
+		
+		window.onpopstate = loadContent;
+	})();
+	
 	
 	function loadLinks( elements ) {
 		if( elements != null ) forEachElement( elements, function( element ) {
-			element.addEventListener( 'click', function( event ) {
-				event.preventDefault() ;
-				
-				linkElements = document
-						.getElementsByClassName( '__eth-selected-link' );
-				forEachElement( linkElements, function( element ) {
-					element.classList.remove( '__eth-selected-link' );
-				} );
-				
-				element.classList.add( '__eth-selected-link' );
+			element.onclick = function( event ) {
+				event.preventDefault();
 				
 				history.pushState( '', '', element.getAttribute( 'href' ) );
-				loadContent() ;
-			} );
+				loadContent();
+			};
+		} );
+	}
+	
+	function changeNavSelectedLink() {
+		var linkElements = document.getElementsByClassName( '__eth-link' );
+		var actualDir = window.location.pathname.substring( 1 );
+		forEachElement( linkElements, function( element ) {
+			element.classList.remove( '__eth-selected-link' );
+			
+			var elementDir = element.getAttribute( 'href' ).substring( 1 );
+			if( elementDir ==  actualDir )
+				element.classList.add( '__eth-selected-link' );
 		} );
 	}
 	
 	function loadContent() {
-		scrollToTop() ;
+		scrollToTop();
+		changeNavSelectedLink();
 		
-		var request = new XMLHttpRequest() ;
+		var request = new XMLHttpRequest();
 		request.open( 'GET', getRealUrl() , true );
 		
 		var contentWrapper = document.getElementById( '__eth-content' );
@@ -36,15 +46,16 @@
 			if ( this.status >= 200 && this.status < 400 ) {
 				var response = this.response;
 				var fn = function()  {
-					contentWrapper.removeEventListener( 'transitionend', fn );
+					contentWrapper.removeEventListener( 'transitionend', fn,
+							true );
 					
 					contentWrapper.innerHTML = response;
-					loadContentLinks() ;
-					loadContentScripts() ;
+					loadContentLinks();
+					loadContentScripts();
 
 					contentWrapper.style.opacity = 1;
 				}
-				contentWrapper.addEventListener( 'transitionend', fn );
+				contentWrapper.addEventListener( 'transitionend', fn, true );
 			} else
 				console.log( 'Ethenis->loadContent()  Error:' + this.status );
 		};
@@ -52,7 +63,7 @@
 			console.log( 'Ethenis->loadContent()  FatalError' );
 		};
 
-		request.send() ;
+		request.send();
 	}
 	
 	function loadContentScripts() {
