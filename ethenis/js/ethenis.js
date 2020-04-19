dbb418c0487b2061c072a74085354ef73b8d6253
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-04-19 02:38:41 +0200

    Remaster javascript library

--- ethenis/js/ethenis.js
+++ ethenis/js/ethenis.js
@@ -1,175 +1,144 @@
+/* eslint-disable curly */
+
 'use strict'
 
-;((ethenis, config) => {
-  ;(history => {
-    let pushState = history.pushState
-    history.pushState = function () {
-      pushState.apply(history, arguments)
-      const pathName = location.pathname
-      if (pathName.endsWith('/'))
-        history.replaceState('', '', pathName.slice(0, -1))
-      loadContent()
-    }
-  })(window.history)
-
-  function loadLinks (elements) {
-    if (elements instanceof HTMLElement) elements = [elements]
-    let numElements = elements !== undefined ? (elements.length !== undefined ? elements.length : 0) : 0
-    for (let i = 0; i < numElements; i++) {
-      let element = (elements[i] || elements)
-      element.addEventListener('click', event => {
-        event.preventDefault()
-        history.pushState('', '', element.getAttribute('href'))
-      }, false)
-    }
-  }
-
-  let loadContent = (() => {
-    let previousLocation = location.pathname
-    return function (reload = false) {
-      if (previousLocation !== location.pathname || reload) {
-        scrollToTop(() => {
-          document.getElementById('__eth-content').style.opacity = '0'
-          setTimeout(() => {
-            document.getElementById('__eth-content').dispatchEvent(new Event('fadetransitionend'))
-          }, config.fadeAnimationDuration)
-        })
-
-        execOnPageChangeFunction()
-        changeNavSelectedLink()
-        document.body.classList.add('loading')
-
-        let request = new XMLHttpRequest()
-        let path = location.pathname +
-          '?ajax=true&_=' + new Date().getTime()
-
-        request.open('GET', path, true)
-        request.onload = () => { requestOnload(request) }
-        request.onerror = () => {
-          console.error('Ethenis->loadContent()  Network Error')
-        }
-        request.onreadystatechange = () => {
-          if (request.readyState === 4) {
-            document.body.classList.remove('loading')
-          }
-        }
-        request.send()
-      }
-      previousLocation = location.pathname
-    }
-  })()
-
-  function reloadContent () {
-    loadContent(true)
-  }
-
-  function scrollToTop (fnc) {
-    if (isNaN(config.scrollAnimationDuration) || config.scrollAnimationDuration <= 0) {
-      window.scrollTo(0, 0)
-      fnc()
-      return
-    }
-
-    let minStep = 0.01
-    let scrollStep = Math.max(window.scrollY / (config.scrollAnimationDuration / 15), minStep)
-
-    let scrollInterval = setInterval(() => {
-      if (window.scrollY > 0) window.scrollBy(0, -scrollStep)
-      else {
-        clearInterval(scrollInterval)
-        fnc()
-      }
-    }, 15)
-  }
-
-  function changeNavSelectedLink () {
-    let linkElements = document.getElementsByClassName('__eth-link')
-    let actualDir = location.pathname.substring(1)
-    ;[].forEach.call(linkElements, element => {
-      element.classList.remove('__eth-selected-link')
-
-      let elementDir = element.getAttribute('href').substring(1)
-      if (elementDir === actualDir) {
-        element.classList.add('__eth-selected-link')
-      }
+;(function (ethenis, config) {
+  const $ = query => document.querySelector(query)
+  const $$ = query => document.querySelectorAll(query)
+  const load = path => {
+    document.body.classList.add('loading')
+    return fetch(path).then(response => {
+      document.body.classList.remove('loading')
+      return response.text()
     })
   }
 
-  function requestOnload (request) {
-    let contentWrapper = document.getElementById('__eth-content')
+  Object.assign(ethenis, {
+    onLoad: ethenis.onLoad,
+    onLoadOnce: ethenis.onLoadOnce,
+    onPageChange: ethenis.onPageChange,
 
-    function showContent () {
-      contentWrapper.removeEventListener('fadetransitionend', showContent, false)
+    reloadContent: () => { ethenis._loadContent(true) },
 
-      contentWrapper.innerHTML = request.response
-      contentWrapper.style.opacity = 1
-      loadContentScripts()
-      loadContentLinks()
-      execOnLoad()
-    }
+    loadLinks: elements => {
+      if (elements instanceof HTMLElement)
+        elements = [elements]
 
-    if (request.status >= 200 && request.status <= 404) {
-      let contentWrapperStyle = window.getComputedStyle(contentWrapper)
-      if (contentWrapperStyle.getPropertyValue('opacity') === '0') {
-        showContent()
-      } else contentWrapper.addEventListener('fadetransitionend', showContent, false)
-    } else { console.error('Ethenis->requestOnload()  Error: ' + request.status) }
-  }
+      ;[...elements].forEach(element => {
+        element.addEventListener('click', event => {
+          event.preventDefault()
+          let href = element.getAttribute('href')
+          if (href !== '/')
+            href = href.replace(/\/+$/, '')
+          history.pushState('', '', href)
+          ethenis._loadContent()
+        })
+      })
+    },
 
-  function execOnPageChangeFunction () {
-    if (typeof ethenis.onPageChange === 'function') ethenis.onPageChange()
-    ethenis.onPageChange = null
-  }
+    _start: () => {
+      if ('scrollRestoration' in history)
+        history.scrollRestoration = 'manual'
 
-  function execOnLoad () {
-    if (typeof ethenis.onLoad === 'function') ethenis.onLoad()
-    if (typeof ethenis.onLoadOnce === 'function') {
-      ethenis.onLoadOnce()
-      ethenis.onLoadOnce = null
-    }
-  }
+      window.addEventListener('popstate', () => { ethenis._loadContent(false) })
+      ethenis.loadLinks($$('.__eth-link'))
+      ethenis._execOnLoad()
+    },
 
-  function loadContentLinks () {
-    let linkElements = document.querySelectorAll('#__eth-content .__eth-link')
-
-    loadLinks(linkElements)
-  }
-
-  function loadContentScripts () {
-    let scripts = document.getElementById('__eth-content')
-      .getElementsByTagName('script')
-
-    _loadContentScripts(scripts, 0)
-  }
-
-  function _loadContentScripts (scripts, i) {
-    if (i >= scripts.length) return
-    let actualScript = scripts[i]
-    let script = document.createElement('script')
-
-    if (actualScript.src) {
-      let request = new XMLHttpRequest()
-      request.open('GET', actualScript.src)
-      request.onload = () => {
-        script.textContent = request.response
-        document.head.appendChild(script)
-          .parentNode.removeChild(script)
-        _loadContentScripts(scripts, ++i)
+    _loadContent: (() => {
+      const changeNavSelectedLink = () => {
+        const actualDir = location.pathname.substring(1)
+        $$('.__eth-link').forEach(element => {
+          element.classList.remove('__eth-selected-link')
+          const elementDir = element.getAttribute('href').substring(1)
+          if (elementDir === actualDir) {
+            element.classList.add('__eth-selected-link')
+          }
+        })
       }
-      request.send()
-    } else {
-      script.textContent = actualScript.textContent
-      document.head.appendChild(script)
-        .parentNode.removeChild(script)
-      _loadContentScripts(scripts, ++i)
+
+      const animatePageChange = () => new Promise(resolve => {
+        const scrollToTop = () => new Promise(resolve => {
+          const ms = 1000 / 60
+          const scrollStep =
+            Math.ceil(window.scrollY / config.scrollAnimationDuration * ms)
+          const scrollInterval = setInterval(() => {
+            if (window.scrollY > 0) {
+              window.scrollBy(0, -scrollStep)
+            } else {
+              clearInterval(scrollInterval)
+              resolve()
+            }
+          }, ms)
+        })
+
+        const endAnimation = () => {
+          $('#__eth-content').style.opacity = 1
+        }
+
+        scrollToTop().then(() => {
+          $('#__eth-content').style.opacity = 0
+          setTimeout(resolve, config.fadeAnimationDuration, endAnimation)
+        })
+      })
+
+      const loadContentLinks = () => {
+        ethenis.loadLinks($$('#__eth-content .__eth-link'))
+      }
+
+      const loadContentScripts = () => {
+        return Promise.all([...$$('#__eth-content script')].map(scriptElement =>
+          scriptElement.src
+            ? load(scriptElement.src)
+            : scriptElement.textContent
+        )).then(([...scripts]) => {
+          scripts.forEach(textContent => {
+            const scriptElement =
+              Object.assign(document.createElement('SCRIPT'), { textContent })
+            document.head
+              .appendChild(scriptElement)
+              .parentNode.removeChild(scriptElement)
+          })
+        })
+      }
+
+      let previousLocation = location.pathname
+
+      return (reload = false) => {
+        if (previousLocation === location.pathname && reload === false)
+          return
+
+        previousLocation = location.pathname
+        ethenis._execOnPageChange()
+        changeNavSelectedLink()
+        const animationPromise = animatePageChange()
+        const fetchPromise =
+          load(location.pathname + '?ajax=true&_=' + new Date().getTime())
+        Promise.all([fetchPromise, animationPromise])
+          .then(([html, animationCallback]) => {
+            $('#__eth-content').innerHTML = html
+            loadContentLinks()
+            loadContentScripts()
+              .then(ethenis._execOnLoad)
+            animationCallback()
+          })
+      }
+    })(),
+
+    _execOnLoad: () => {
+      if (typeof ethenis.onLoad === 'function')
+        ethenis.onLoad()
+      if (typeof ethenis.onLoadOnce === 'function')
+        ethenis.onLoadOnce()
+      ethenis.onLoadOnce = null
+    },
+
+    _execOnPageChange: () => {
+      if (typeof ethenis.onPageChange === 'function')
+        ethenis.onPageChange()
+      ethenis.onPageChange = null
     }
-  }
+  })
 
-  loadLinks(document.getElementsByClassName('__eth-link'))
-  ethenis.loadLinks = loadLinks
-  ethenis.reloadContent = reloadContent
-  if ('scrollRestoration' in history) history.scrollRestoration = 'manual'
-  execOnLoad()
-  window.addEventListener('popstate', () => { loadContent() }, true)
-
+  ethenis._start()
 })(__ETHENIS, __ETHENIS.config)
